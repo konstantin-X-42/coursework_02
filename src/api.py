68ac8718d7a69d9d50fd80fc4b7721da7a9e58cd
konstantin <kostyani4y@yandex.ru>
2026-07-23 00:05:06 +0300

    корректировка api.py

--- src/api.py
+++ src/api.py
@@ -159,7 +159,7 @@ class APIAdapter(BaseAPIAdapter):
                 print()
                 print(f"✅ Получены реальные данные OpenSky.")
                 print(f"✈️ Самолетов найдено в воздушном пространстве: {count}")
-                print("    Обновляем данные ...")
+                print("    Загрузка данных ...")
 
             else:
                 print(f"❌ OpenSky ошибка: {response.status_code}")
